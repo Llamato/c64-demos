b1458ccdf4235b59e4dec845ccd11e24c8f0bf15
llamato <tguessbacher@gmx.de>
2026-09-11 09:35:18 +0200

    seperated llvm/c and acme/asm devShells

--- flake.nix
+++ flake.nix
@@ -21,80 +21,88 @@
         "aarch64-linux"
         "riscv64-linux"
       ];
-    allSystems = linuxSystems ++ darwinSystem;
+      allSystems = linuxSystems ++ darwinSystem;
     in
     inputs.flake-utils.lib.eachSystem linuxSystems (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
         lib = pkgs.lib;
-        llvm-mos-sdk = inputs.dotfiles-llamato.packages.${system}.llvm-mos-sdk;
+        llvm-mos-sdk = inputs.nixpkgs-llamato.packages.${system}.llvm-mos-sdk;
         psid = inputs.dotfiles-llamato.packages.${system}.psid;
         vchar64 = inputs.dotfiles-llamato.packages.${system}.vchar64;
         metaOf = name: {
           description = "";
           maintainers = with lib.maintainers; [ llamato ];
         };
-        acme-build = name: pkgs.stdenv.mkDerivation {
-          name = "${name}-acme";
-          version = "0.0.1";
-          meta = metaOf name;
-          src = ./${name};
-          buildPhase = ''
-            runHook preBuild
-            ${pkgs.acme}/bin/acme --cpu 6510 --format cbm -o ${name}.prg main.asm
-            runHook postBuild
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp ${name}.prg $out
-          '';
-        };
-        basic-build = name: pkgs.stdenv.mkDerivation {
-          name = "${name}-basic";
-          version = "0.0.1";
-          meta = metaOf name;
-          src = ./${name};
-          buildPhase = ''
-            runHook preBuild
-            find . -name "*.bas" -execdir sh -c '${pkgs.vice}/bin/petcat -w2 -o $1.prg -- $1' sh {} \;
-            runHook postBuild
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp *.bas.prg $out
-          '';
-        };
-        binary-build = name: pkgs.stdenv.mkDerivation {
-          name = "${name}-bins";
-          version = "0.0.1";
-          meta = metaOf name;
-          src = ./${name};
-          installPhase = ''
-            mkdir -p $out
-            cp *.bin $out
-          '';
-        };
-        disk-build = paths: name: pkgs.stdenv.mkDerivation {
-          name = "${name}-d64";
-          version = "0.0.1";
-          meta = metaOf name;
-          src = pkgs.symlinkJoin {
-            inherit name;
-            inherit paths;
+        acme-build =
+          name:
+          pkgs.stdenv.mkDerivation {
+            name = "${name}-acme";
+            version = "0.0.1";
+            meta = metaOf name;
+            src = ./${name};
+            buildPhase = ''
+              runHook preBuild
+              ${pkgs.acme}/bin/acme --cpu 6510 --format cbm -o ${name}.prg main.asm
+              runHook postBuild
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp ${name}.prg $out
+            '';
           };
-          buildPhase = ''
-            ${pkgs.vice}/bin/c1541 -format ${name},0 d64 ${name}.d64
-            find . -name "*.prg" -a \! \( -name "*.bas.*" \) -execdir sh -c '${pkgs.vice}/bin/c1541 -attach "${name}.d64" -write "$1" "$(basename "$1" .prg)"' sh {} \;
-            find . -name "*.bas.prg" -execdir sh -c '${pkgs.vice}/bin/c1541 -attach "${name}.d64" -write "$1" "$(basename "$1" .bas.prg)"' sh {} \;
-            find . -name "*.seq" -execdir sh -c '${pkgs.vice}/bin/c1541 -attach ${name}.d64 -write "$1" "$(basename "$1" .seq)"' sh {} \;
-            find . -name "*.bin" -execdir sh -c '${pkgs.vice}/bin/c1541 -attach ${name}.d64 -write "$1" "$(basename "$1" .bin)"' sh {} \;
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp ${name}.d64 $out
-          '';
-        };
+        basic-build =
+          name:
+          pkgs.stdenv.mkDerivation {
+            name = "${name}-basic";
+            version = "0.0.1";
+            meta = metaOf name;
+            src = ./${name};
+            buildPhase = ''
+              runHook preBuild
+              find . -name "*.bas" -execdir sh -c '${pkgs.vice}/bin/petcat -w2 -o $1.prg -- $1' sh {} \;
+              runHook postBuild
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp *.bas.prg $out
+            '';
+          };
+        binary-build =
+          name:
+          pkgs.stdenv.mkDerivation {
+            name = "${name}-bins";
+            version = "0.0.1";
+            meta = metaOf name;
+            src = ./${name};
+            installPhase = ''
+              mkdir -p $out
+              cp *.bin $out
+            '';
+          };
+        disk-build =
+          paths: name:
+          pkgs.stdenv.mkDerivation {
+            name = "${name}-d64";
+            version = "0.0.1";
+            meta = metaOf name;
+            src = pkgs.symlinkJoin {
+              inherit name;
+              inherit paths;
+            };
+            buildPhase = ''
+              ${pkgs.vice}/bin/c1541 -format ${name},0 d64 ${name}.d64
+              find . -name "*.prg" -a \! \( -name "*.bas.*" \) -execdir sh -c '${pkgs.vice}/bin/c1541 -attach "${name}.d64" -write "$1" "$(basename "$1" .prg)"' sh {} \;
+              find . -name "*.bas.prg" -execdir sh -c '${pkgs.vice}/bin/c1541 -attach "${name}.d64" -write "$1" "$(basename "$1" .bas.prg)"' sh {} \;
+              find . -name "*.seq" -execdir sh -c '${pkgs.vice}/bin/c1541 -attach ${name}.d64 -write "$1" "$(basename "$1" .seq)"' sh {} \;
+              find . -name "*.bin" -execdir sh -c '${pkgs.vice}/bin/c1541 -attach ${name}.d64 -write "$1" "$(basename "$1" .bin)"' sh {} \;
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp ${name}.d64 $out
+            '';
+          };
         demos = {
           kneedeepin3d = pkgs.stdenv.mkDerivation {
             name = "kneedeepin3d";
@@ -116,7 +124,11 @@
           random = acme-build "random";
           sidplayer = acme-build "sidplayer";
           kneedeepin2d = acme-build "kneedeepin2d";
-          charsets = disk-build [(acme-build "charsets") (basic-build "charsets") (binary-build "charsets")] "charsets";
+          charsets = disk-build [
+            (acme-build "charsets")
+            (basic-build "charsets")
+            (binary-build "charsets")
+          ] "charsets";
         };
       in
       {
@@ -127,23 +139,27 @@
           };
         }
         // demos;
-        apps = builtins.mapAttrs (name: drv: 
-        {
-            type = "app";
-            program = "${pkgs.writeShellScript "run-${name}" ''exec ${pkgs.vice}/bin/x64sc $(find ${drv}/ -name "${name}.prg" -o -name "${name}.d64" | head -1) "$@"''}";
-            meta = metaOf name;
-          }) demos;
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            acme
-            vice
-            rehex
-            sidplayfp
-            llvm-mos-sdk
-            psid
-            vchar64
-          ];
-        };
+        apps = builtins.mapAttrs (name: drv: {
+          type = "app";
+          program = "${pkgs.writeShellScript "run-${name}" ''exec ${pkgs.vice}/bin/x64sc $(find ${drv}/ -name "${name}.prg" -o -name "${name}.d64" | head -1) "$@"''}";
+          meta = metaOf name;
+        }) demos;
+        devShells =
+          let
+            packagesByDevShell = rec {
+              default = with pkgs; [
+                vice
+                rehex
+                sidplayfp
+                psid
+                vchar64
+                acme
+              ];
+              cc = default ++ [llvm-mos-sdk];
+            };
+          in builtins.mapAttrs (_: packages: pkgs.mkShell {
+            packages = packages;
+          }) packagesByDevShell;
       }
     );
 }
